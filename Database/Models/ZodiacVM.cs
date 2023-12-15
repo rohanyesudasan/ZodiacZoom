aa1720f7f883102132789e6d3f32f65990a4ea3b
Rohan <rohanyesudasan@gmail.com>
2023-12-16 05:08:53 +0530

    added feature where user can get his zodiac after entering birthdate

---
+++ Database/Models/ZodiacVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Models
+{
+    public class ZodiacVM
+    {
+        public DateOnly BirthDate { get; set; }
+        public string? ZodiacSign { get; set; }
+    }
+}
--- zodiac/Controllers/ZodiacController.cs
+++ zodiac/Controllers/ZodiacController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Database.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace zodiac.Controllers
 {
     public class ZodiacController : Controller
     {
+        private ZodiacContext _DbContext { get; set; }
+        public ZodiacController(ZodiacContext zodiacContext)
+        {
+            _DbContext = zodiacContext;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ZodiacVM zodiacVM)
+        {
+            var yearDiff = zodiacVM.BirthDate.Year - 1970;
+            zodiacVM.ZodiacSign = _DbContext.Zodiacs
+            .Single(x => zodiacVM.BirthDate >= x.FromDate.AddYears(yearDiff)
+                && zodiacVM.BirthDate <= x.ToDate.AddYears(yearDiff))
+            .ZodiacSign;
+
+
+            return View(zodiacVM);
         }
     }
 }
